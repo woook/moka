34abff563e14ef4971fd20689049fbe4b3d903ee
Aled Jones <aledjones@nhs.net>
2017-11-09 17:47:48 +0000

    Create ImportPrimerConfig.py

---
+++ code/PrimerDB/ImportPrimerConfig.py
@@ -0,0 +1,16 @@
+# location of primer design files to import
+primer_design_files="\\\\gstt.local\\shared\\Genetics\\DNA LAB\\Primers\\MokaPrimerDatabase\\primer_designs"
+# location of imported design files 
+archived_primer_design_files=primer_design_files+"\\imported_primers\\"
+
+# Use of bed file to get gene symbol
+pan493_bedfile="\\\\gstt.local\\Apps\\Moka\\Files\\Software\\PrimerDB\\Pan493dataSambamba.bed"
+
+#default strings to import to database
+genome_build_text="GRCH37"
+scale_of_synth_text="0.025"
+purification_text="Desalt"
+status_string="Designed - awaiting ordering"
+# default tag values
+f_tag="1"
+r_tag="2"
